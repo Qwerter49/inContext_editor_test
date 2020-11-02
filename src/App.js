163e1ae22c46cdbb182077ed33faba245c479a61
Jay Oliver <jaykamas@gmail.com>
2020-11-02 09:43:21 -0500

    inContext editor test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import highlighter from './highlighter.svg'
+import eraser from './eraser.svg'
 import './App.css';
 
 function App() {
+
+  const [cursor, setCursor] = useState('pointer')
+
+  const changeEraserHeight = (event) => {
+    if(event.target.className === "eraser"){
+      event.target.className = "eraserClicked"
+      document.querySelector('.App').className = "AppEraserClicked"
+    } else {
+      event.target.className= 'eraser'
+      document.querySelector('.AppEraserClicked').className = "App"
+    }
+  }
+  const changeHighlightHeight = (event) => {
+    if(event.target.className === "highlighter"){
+      event.target.className = "highlighterClicked"
+      document.querySelector('.App').className = "AppHighlightClicked"
+
+    } else {
+      event.target.className= 'highlighter'
+      document.querySelector('.AppHighlightClicked').className = "App"
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React App</h1>
+      <div>Icons made by <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+      <div className="editor-container">
+        <img src={eraser} className="eraser" onClick={changeEraserHeight} alt="Highlighter Logo"/>
+        <img src={highlighter} className="highlighter" onClick={changeHighlightHeight} alt="Highlighter Logo"/>
+      </div>
     </div>
   );
 }
